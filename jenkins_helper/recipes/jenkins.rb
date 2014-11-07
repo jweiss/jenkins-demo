5861f18e7aed26b0b7e9fbdad9b2f499b285668f
Jonathan Weiss <jw@innerewut.de>
2014-11-07 15:50:50 +0100

    ID

--- jenkins_helper/recipes/jenkins.rb
+++ jenkins_helper/recipes/jenkins.rb
@@ -1,13 +1,3 @@
-template '/var/lib/jenkins/config.xml' do
-  source 'global.xml.erb'
-  variables :environment_variables => {
-    "OpsWorksInstanceID" => node[:opsworks][:instance][:id]
-  }
-  owner 'jenkins'
-  group 'jenkins'
-  mode 0644
-end
-
 jenkins_plugin 'ruby'
 jenkins_plugin 'rake'
 
