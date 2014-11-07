83f3b84cad36ca7f66ce2ca92153d5c42ad43d2a
Jonathan Weiss <jw@innerewut.de>
2014-11-07 12:17:15 +0100

    Install AWS SDK Ruby

--- jenkins_helper/recipes/jenkins.rb
+++ jenkins_helper/recipes/jenkins.rb
@@ -4,3 +4,7 @@ jenkins_plugin 'rake'
 jenkins_plugin 'git' do
   notifies :restart, 'service[jenkins]', :immediately
 end
+
+[{'awsk-sdk' => nil}].each do |gem_name, version|
+  execute "/usr/local/bin/gem install --no-ri --no-rdoc #{gem_name} #{"--version #{version}" if version}"
+end
