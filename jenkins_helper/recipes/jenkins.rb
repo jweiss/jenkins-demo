efef58a40d1b06ec91efc2b60ebeb0bf183c6842
Jonathan Weiss <jw@innerewut.de>
2014-11-07 12:23:26 +0100

    fix typo

--- jenkins_helper/recipes/jenkins.rb
+++ jenkins_helper/recipes/jenkins.rb
@@ -5,6 +5,6 @@ jenkins_plugin 'git' do
   notifies :restart, 'service[jenkins]', :immediately
 end
 
-{'awsk-sdk' => nil}.each do |gem_name, version|
+{'aws-sdk' => nil}.each do |gem_name, version|
   execute "/usr/local/bin/gem install --no-ri --no-rdoc #{gem_name} #{"--version #{version}" if version}"
 end
